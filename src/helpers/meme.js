a199b88dc0d74524aaca2b3102d884533abd3976
Anna Liberty <liberty.817@proton.me>
2026-01-20 12:24:39 -0800

    Nuke JavaScript environment

--- src/helpers/meme.js
+++
@@ -1,8 +0,0 @@
-// helpers/meme.js
-import { EmbedBuilder } from "discord.js";
-
-const MEME_URL = process.env.MEME_URL || "";
-
-export const getMeme = async () => {
-  return new EmbedBuilder().setImage(MEME_URL);
-};
